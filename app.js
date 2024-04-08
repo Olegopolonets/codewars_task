2fdd871dbb4b2add5adf3c5c792bc4778537631e
Olegopolonets <54370738+Olegopolonets@users.noreply.github.com>
2024-04-09 01:19:55 +0300

    remove code

--- app.js
+++ app.js
@@ -541,14 +541,14 @@
  *  FizzBuzz
  */
 
-for (let i = 1; i <= 100; i += 1) {
-  if (i % 5 === 0 && i % 3 === 0) {
-    console.log("FizzBuzz");
-  } else if (i % 5 === 0) {
-    console.log("Buzz");
-  } else if (i % 3 === 0) {
-    console.log("Fizz");
-  } else {
-    console.log(i);
-  }
-}
+// for (let i = 1; i <= 100; i += 1) {
+//   if (i % 5 === 0 && i % 3 === 0) {
+//     console.log("FizzBuzz");
+//   } else if (i % 5 === 0) {
+//     console.log("Buzz");
+//   } else if (i % 3 === 0) {
+//     console.log("Fizz");
+//   } else {
+//     console.log(i);
+//   }
+// }
